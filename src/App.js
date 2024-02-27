af49422c50103a8a8a38584cc3e0875433584cd5
retardedE <par.emad@yahoo.com>
2024-02-27 03:45:05 -0800

    data fetch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
+import { styled, Box } from '@mui/material';
+import { alpha, InputBase } from '@mui/material';
+import SearchIcon from "@mui/icons-material/Search";
+const apiKey = "c2f46938";
 function App() {
+  const [query,setQuery]=useState('')
+  const [search,setSearch]=useState('')
+  const fetchData = (movieTitle) =>{
+    fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=${apiKey}&s=${movieTitle}`)
+    .then(response => response.json())
+    .then(data => setQuery(data.Search))
+  }
+  useEffect(()=>{
+    fetchData(search)
+  },[search])
+  const HeadingWrapper = styled("h1")(({theme})=>({
+    display:"flex",
+    flexDirection:"column",
+    justifyContent:"center",
+    alignItems:"center",
+    fontSize:"48px",
+    marginBottom:theme.spacing(4),
+    color:theme.palette.primary.textColor,
+  }))
+  console.log(query)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    App
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,9 @@
+import { createTheme } from "@mui/material";
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#2b2a2a",
+      textColor: "#c2bebe",
+    },
+  },
+});
